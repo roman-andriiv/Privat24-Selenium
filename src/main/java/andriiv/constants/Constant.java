9570fc0511c3526cfe8e938d7c1561943a3c664c
Roman Andriiv <roman.andriyiv@gmail.com>
2023-01-24 18:56:27 +0100

    prepare BasePage for testing

--- src/main/java/andriiv/constants/Constant.java
+++ src/main/java/andriiv/constants/Constant.java
@@ -4,5 +4,6 @@ public class Constant {
     public static class TimeoutVariables {
 
         public static final int IMPLICIT_WAIT = 5;
+        public static final int EXPLICIT_WAIT = 10;
     }
 }
--- src/main/java/andriiv/pages/base/BasePage.java
+++ src/main/java/andriiv/pages/base/BasePage.java
@@ -1,11 +1,34 @@
 package andriiv.pages.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static andriiv.constants.Constant.TimeoutVariables.EXPLICIT_WAIT;
 
 public class BasePage {
     WebDriver driver;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    /**
+     * The method for navigating to a specific URL
+     */
+    public void goToUrl(String url) {
+        driver.get(url);
+    }
+
+    /**
+     * Wait for the element visibility on DOM model
+     */
+    public WebElement waitElementIsVisible(WebElement element){
+        new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT)).until(ExpectedConditions.visibilityOf(element));
+        return element;
     }
 }
